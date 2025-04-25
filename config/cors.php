23cd5cf3a375de7c9ffa4031ad3e5ba36e5e25c0
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-25 16:28:34 +0400

    update CORS configuration to include additional allowed origins

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', '*'],
+    'allowed_origins' => ['http://localhost:5173', 'https://my-blog-backend-production-197d.up.railway.app', 'https://my-blog-frontend-mocha.vercel.app', '*'],
 
     'allowed_origins_patterns' => [],
 
